25f69dcab0de1ae7b4b8b78b582b3689f6b48c04
PhillipTruong <truonghphillip@gmail.com>
2021-12-01 17:05:15 -0500

    added fake auth and dashboard component

--- frontend/enershare/src/app.js
+++ frontend/enershare/src/app.js
@@ -1,11 +1,42 @@
-// import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom' 
+import decode from 'jwt-decode'
 
 import { Title } from './components/testComponent/test.sc' 
 import Login  from './pages/login/login'
 import SignUp from './pages/signup/signup'
+import Dashboard from './pages/dashboard/dashboard'
 
 // import Home  from './pages/home/home'
+
+const checkAuth = () => {
+  // const token = localStorage.getItem('token')
+  // const refreshToken = localStorage.getItem('refreshToken')
+  // if (!token || !refreshToken){
+  //   return false
+  // }
+
+  // try {
+  //   const payload = decode(refreshToken)
+  //   if (payload.exp < new Date().getTime()/1000){
+  //     return true
+  //   }
+  // } catch (error) {
+  //   return false
+  // }
+  return true
+}
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+      checkAuth ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/login" }} />
+      )
+    )} />
+)
+
 
 const App = () => (
   <Router>
@@ -19,6 +50,7 @@ const App = () => (
       <Route exact path="/signUp">
         <SignUp/>
       </Route>
+      <AuthRoute exact path="/dashboard" component={Dashboard}/>
       {/* <Route exact path="/home">
         <Home />
       </Route> */}
