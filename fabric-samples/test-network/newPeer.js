135362cd2c0a236ec5b3f3215b74144058c7902b
Christopher Devito <cdevito2@uwo.ca>
2022-02-24 19:24:07 -0500

    removing hardcoded sections of new scripts

--- fabric-samples/test-network/newPeer.js
+++ fabric-samples/test-network/newPeer.js
@@ -3,14 +3,99 @@ const fs = require('fs');
 const { syncBuiltinESMExports } = require('module');
 const exec = require('child_process').execSync;
 
+var newPeerName = 'chris'
+console.log(newPeerName+".org1.example.com")
+
+
+
+/* this would ideally be function that new peer calls*/
+
+function createNewPeer(newPeerName, newPeerCorePort){
+    //const myShellScript = exec('./organizations/fabric-ca/generatePeerCrypto.sh '+newPeerName); //pass newPeerName as argument to the bash script
+    var peerAbbreviation = newPeerName+'.org1.example.com'
+    let new_peer_data = {
+        version: '3.7',
+        volumes: {
+            "peerAbbreviation":null
+        }, //this line is causing me problems
+
+        networks: { test: { name: 'fabric_test' } },
+        services:{
+            peerAbbreviation:{
+                container_name: peerAbbreviation,
+                image: 'hyperledger/fabric-peer:latest',
+                labels: { service: 'hyperledger-fabric' },
+                environment: [
+                'CORE_VM_ENDPOINT=unix:///host/var/run/docker.sock',
+                'CORE_VM_DOCKER_HOSTCONFIG_NETWORKMODE=fabric_test',
+                'FABRIC_LOGGING_SPEC=INFO',
+                'CORE_PEER_TLS_ENABLED=true',
+                'CORE_PEER_PROFILE_ENABLED=false',
+                'CORE_PEER_TLS_CERT_FILE=/etc/hyperledger/fabric/tls/server.crt',
+                'CORE_PEER_TLS_KEY_FILE=/etc/hyperledger/fabric/tls/server.key',
+                'CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/fabric/tls/ca.crt',
+                'CORE_PEER_ID='+peerAbbreviation,
+                'CORE_PEER_ADDRESS='+peerAbbreviation+':'+newPeerCorePort,
+                'CORE_PEER_LISTENADDRESS=0.0.0.0:'+newPeerCorePort,
+                'CORE_PEER_CHAINCODEADDRESS='+peerAbbreviation+':'+newPeerCorePort+1,
+                'CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:'+newPeerCorePort+1,
+                'CORE_PEER_GOSSIP_BOOTSTRAP='+peerAbbreviation+':'+newPeerCorePort,
+                'CORE_PEER_GOSSIP_EXTERNALENDPOINT='+peerAbbreviation+':'+newPeerCorePort,
+                'CORE_PEER_LOCALMSPID=Org1MSP',
+                'CORE_OPERATIONS_LISTENADDRESS=0.0.0.0:'+newPeerCorePort+10000
+                ],
+                volumes: [
+                //'/var/run/:/host/var/run/docker.sock',
+                '/var/run/docker.sock:/host/var/run/docker.sock',
+                '../organizations/peerOrganizations/org1.example.com/peers/'+peerAbbreviation+'/msp:/etc/hyperledger/fabric/msp',
+                '../organizations/peerOrganizations/org1.example.com/peers/'+peerAbbreviation+'/tls:/etc/hyperledger/fabric/tls',
+                peerAbbreviation+':/var/hyperledger/production'
+                ],
+                working_dir: '/opt/gopath/src/github.com/hyperledger/fabric/peer',
+                command: 'peer node start',
+                ports: [ newPeerCorePort+':'+newPeerCorePort, (10000+newPeerCorePort)+':'+(newPeerCorePort+10000) ],
+                networks: [ 'test' ]
+                }
+        }
+    }
+
+    //create the new yaml file
+    fs.writeFileSync('./docker/docker-compose-'+newPeerName+'.yaml', yaml.dump(new_peer_data,{lineWidth: -1}), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    const newCmd = exec('export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp');
+    //bring up the new peer container
+    exec('docker-compose -f ./docker/docker-compose-'+newPeerName+'.yaml up -d',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+
+    //join the channel and install chaincode on new peer
+    const joinChannelCmd = exec('./scripts/dynamicPeerJoinChannel.sh '+newPeerName+" "+newPeerCorePort);
+
+}
+
+
+
+
+createNewPeer('peer3',9051)
+
+/* USE BELOW FOR TESTING FOR NOW, AUTO CREATES PEER CALLED PEER3, FOR APP WE DONT WANNA HARDCODE PEER NAME*/
+/*
 try{
 
     
-    /* STEP 1 GENERATE CRYPTO MATERIAL FOR NEW PEER */
-    //const myShellScript = exec('./organizations/fabric-ca/generatePeerCrypto.sh');
+    
+    const myShellScript = exec('./organizations/fabric-ca/generatePeerCrypto.sh'); //TODO: pass new peer name instead of hardcoded peer3
     
     
-    /* STEP 2 CREATE CONFIGURATION FOR THE NEW PEER*/
+    
     
     
 
@@ -72,8 +157,8 @@ try{
 
     const newCmd = exec('export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp');
     
-    //const dockerSockCmd = exec('SOCK="${DOCKER_HOST:-/var/run/docker.sock}" && DOCKER_SOCK="${SOCK##unix://}"')
-    /* STEP 3 BRING UP CONTAINER FOR NEW PEER*/
+    
+    
     
     exec('docker-compose -f ./docker/docker-compose-newpeer.yaml up -d',
     function (error, stdout, stderr) {
@@ -85,7 +170,7 @@ try{
     });
 
     
-    /* STEP 4 - join the new peer to the channel AND INSTALL chaincode*/
+    
     
 
 
@@ -94,10 +179,10 @@ try{
     
 
     
-    /* REMEMBER TO REMOVE VOLUME WHEN YOU ARE DONE WITH IT!*/
+    
     
 }catch(e){
     console.log(e)
 }
-
-
+*/
+/* REMEMBER TO REMOVE VOLUME WHEN YOU ARE DONE WITH IT!*/
--- fabric-samples/test-network/scripts/dynamicPeerJoinChannel.sh
+++ fabric-samples/test-network/scripts/dynamicPeerJoinChannel.sh
@@ -3,12 +3,16 @@
 
 export FABRIC_CFG_PATH=${PWD}/../config/ 
 export PATH=$PATH:${PWD}/../bin/ 
+
+NEW_PEER=${1}
+NEW_PEER_PORT=${2}
+
 export CORE_PEER_TLS_ENABLED=true 
 export CORE_PEER_PROFILE_ENABLED=false 
 export CORE_PEER_LOCALMSPID="Org1MSP" 
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer3.org1.example.com/tls/ca.crt 
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/${NEW_PEER}.org1.example.com/tls/ca.crt 
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp 
-export CORE_PEER_ADDRESS=localhost:9051 
+export CORE_PEER_ADDRESS=localhost:${NEW_PEER_PORT} 
 export CORE_VM_ENDPOINT=unix:///host/var/run/docker.sock
 peer channel join -b ./channel-artifacts/mychannel.block
 peer lifecycle chaincode install basic.tar.gz
